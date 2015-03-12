178711aa2912aecc941faf977f04a73267794453
Sebastian Florez <sflorez@stetson.edu>
2015-03-11 23:12:40 -0400

    Add parse dependencies and initialization

--- app/src/main/java/com/simpleparseloginapp/sebastianflorez/simpleparseloginapp/MainActivity.java
+++ app/src/main/java/com/simpleparseloginapp/sebastianflorez/simpleparseloginapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "7Qpfmqm7lB40IF9MeqWJZI5BKZNkocFIVUitZxtU", "Pgf377zKHoSiJd4bc8tjGdG9diNFWvt0d3R9rNzz");
     }
 
 
